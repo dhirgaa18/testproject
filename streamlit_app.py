67dc00bc02717d010326a4943f0eee7f7496ce4b
dhirgaa18 <dhirgafayzulh@gmail.com>
2025-07-12 08:34:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
 import streamlit as st
 
-st.title("🎈 Raja Kudus 666")
-st.write(
-    "Selamat Datang di Website Raja Meme."
-    "Website ini tidak menyediakan Judol, tetapi menyediakan barang barang yang sangat langka."
-    "Cihuyyyyyy"
-)
+st.write("Hello, World! : sunglasses:")
